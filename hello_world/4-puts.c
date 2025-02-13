b7db513352d2a02b8de252f1ff0b97ea237d93a5
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-13 18:42:30 +0000

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*Funcion principal*/
+/**
+ * main - use puts
+ *
+ * Return: 0 if the function is correct
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
