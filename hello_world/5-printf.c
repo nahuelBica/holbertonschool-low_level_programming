25df7a6da8f3ac52499967d466f9d00360bff483
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-13 18:59:00 +0000

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - use printf
+ *
+ * Return: 0 if the function is correct
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
