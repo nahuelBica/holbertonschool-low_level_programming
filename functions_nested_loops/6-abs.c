2002c029fef4e2533e8da1c60fd9366125277a36
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-18 13:41:19 +0000

    task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_sign - check if c is lowercase
+ *
+ * @n: parameter that obtain a number
+ *Return: 1 if is positive, 0 if zero, -1 if negative
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n*-1);
+	}
+	return (n);
+}
