8419f3852dd49865ba02e336a0fb345eb73920f7
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-18 13:52:13 +0000

    task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *print_last_digit - print last digit of a number
+ *
+ *@n: parameter that obtain a number
+ *Return: last digit of number
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
