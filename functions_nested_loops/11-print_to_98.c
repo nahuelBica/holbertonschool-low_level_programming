d6e1099ae8219d561da681814606b45818c1f63a
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-19 13:07:38 +0000

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,14 +6,16 @@
 */
 void print_to_98(int n)
 {
-	do {
-		if (n >= 98)
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n < 98)
 		{
-			printf("%d\n", 98);
+			n++;
 		} else
 		{
-			printf("%d, ", n);
+			n--;
 		}
-		n++;
-	} while (n <= 98);
+	}
+	printf("%d\n", 98);
 }
