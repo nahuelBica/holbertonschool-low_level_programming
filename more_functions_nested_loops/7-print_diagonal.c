ef4283de6793b1eba28a418a7a96b2d8b6066a46
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-24 15:55:49 +0000

    task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - print diagonal with n \
+ *
+ * @n: number of times \ is printed
+ */
+void print_diagonal(int n)
+{
+	int i, c;
+
+	c = 0;
+	while (n > 0)
+	{
+		for (i = 0; i < c; i++)
+			_putchar (' ');
+		_putchar('\\');
+		_putchar('\n');
+		n--;
+		c++;
+	}
+	_putchar('\n');
+}
