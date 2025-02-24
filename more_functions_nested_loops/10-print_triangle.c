b91c86c9617d44bec1c5c9ca6871b81a848be1ee
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-24 16:52:38 +0000

    task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle with height n
+ *
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int aux = size;
+	int i, c;
+
+	c = 1;
+	while (aux > 0)
+	{
+		for (i = 0; i < size; i++)
+			if (size - c <= i)
+			{
+				_putchar('#');
+			} else
+			{
+				_putchar(' ');
+			}
+		if (size != 1)
+			_putchar('\n');
+		c++;
+		aux--;
+	}
+	_putchar('\n');
+}
