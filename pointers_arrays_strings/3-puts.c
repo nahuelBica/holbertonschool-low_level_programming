c57358affb199ffaed0c90c941b2dbf20e4c4987
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-25 18:27:58 +0000

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ *@str: string
+ */
+
+void _puts(char *str)
+{
+	int n = 0;
+	while (*(str + n) != 0)
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
