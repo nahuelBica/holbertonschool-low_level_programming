d53008c7ddb1930c77e88e17538ae1dd9ed3f5bf
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-25 18:54:40 +0000

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print a reversed string
+ *@s: string
+ */
+void print_rev(char *s)
+{
+	int n, cont = 0;
+
+
+	while (*(s + n) != 0)
+	{
+		cont++;
+		n++;
+	}
+	while (cont > 0)
+	{
+		cont--;
+		_putchar(*(s + cont));
+	}
+	_putchar('\n');
+}
