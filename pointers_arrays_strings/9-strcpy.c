923f760d742c38ffa2a252a6cf9215d6820441c3
Nahuel Bica <nahuelbica5912@gmail.com>
2025-02-27 13:17:09 +0000

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - copy string
+ *@dest: destination
+ *@src: source
+ *Return: dest pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+
+	while (src[n] != 0)
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	return (dest);
+}
