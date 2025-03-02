18826bc6110f7e1b620bf1314ef367385a584da8
Nahuel Bica <nahuelbica5912@gmail.com>
2025-03-02 16:23:44 +0000

    task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _*string_toupper - change to upper
+ *@s: string
+ *Return: length of string s
+ */
+char *string_toupper(char *s)
+{
+	int n = 0;
+
+	while (s[n] != 0)
+	{
+		if (s[n] < 'z' && s[n] > 'a')
+			s[n] -= 32;
+		n++;
+	}
+	return (s);
+}
