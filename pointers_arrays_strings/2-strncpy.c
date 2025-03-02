34ede494daa49724c7af49417919660204716934
Nahuel Bica <nahuelbica5912@gmail.com>
2025-03-02 15:36:44 +0000

    task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i, x;
 
-	while (i < n)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
+	for (x = i; x < n; x++)
+		dest[x] = '\0';
+
+	return dest;
 }
