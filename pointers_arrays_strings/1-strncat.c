c0e114dcdb2d28a4f61d4a3df6c2f04254ad0b40
Nahuel Bica <nahuelbica5912@gmail.com>
2025-03-02 15:11:29 +0000

    task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strncat - cat string src after dest
+ *@dest: destination
+ *@src: source
+ *@n: max length of src
+ *Return: dest pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0, i = 0;
+
+	while (dest[x] != 0)
+	{
+		x++;
+	}
+	while (i < n && src[i] != 0)
+	{
+		dest[x] = src[i];
+		x++;
+		i++;
+	}
+	x++;
+	dest[x] = '\0';
+	return (dest);
+}
