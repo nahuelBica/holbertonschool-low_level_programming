a7c6d525f56baf3e1706d6ef0a370bad41494ef8
Nahuel Bica <nahuelbica5912@gmail.com>
2025-03-03 13:06:15 +0000

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strchr - search char in a string
+ *@s: string
+ *@c: char
+ *Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0, check = 0;
+	char *o = 0;
+
+	while (s[i] != 0 && !check)
+	{
+		if (s[i] == c)
+		{
+			o = &s[i];
+			check = 1;
+		}
+		i++;
+	}
+	return (o);
+}
