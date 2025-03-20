0067ddd5ed00c515325477fdf30b6cabbf9c347e
Nahuel Bica <nahuelbica5912@gmail.com>
2025-03-20 19:27:23 +0000

    task 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * **alloc_grid - 2 dimensional array of ints
+ *@width: width of array
+ *@height: height of array
+ *Return: pointer to array
+ */
+int **alloc_grid(int width, int height)
+{
+	int ln = 0, col = 0;
+	int **ar;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+	ar = malloc(height * sizeof(int));
+	if (!ar)
+		return (0);
+
+	while (ln < height)
+	{
+		ar[ln] = malloc(width * sizeof(int));
+		while (col < width)
+		{
+			ar[ln][col] = 0;
+			col++;
+		}
+		col = 0;
+		ln++;
+	}
+
+	return (ar);
+}
