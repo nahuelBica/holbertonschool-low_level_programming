80a3714141a0c93ef5a4af71303850ebc5c68ec6
Nahuel Bica <nahuelbica5912@gmail.com>
2025-03-21 13:58:19 +0000

    protect double inclusion

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,11 @@
 /**
-*File:main.h
+*File:variadic_functions.h
 *Desc:function declaration
 */
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+
+#endif
