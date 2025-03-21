986e26b4d7b90ef9e85edc6c7b3fc6cdd42aa6f2
Nahuel Bica <nahuelbica5912@gmail.com>
2025-03-21 14:04:02 +0000

    clear comment of v_f.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,11 +2,7 @@
 #define variadic_functions_h
 #include <stdarg.h>
 
-/**
-*File:variadic_functions.h
-*Desc:function declaration
-*/
-int _putchar(char c);
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 
 #endif
